127079cabb66ce110f73d5660ce2ba25f5395588
Olof Bjarnason <olof.bjarnason@gmail.com>
2021-06-23 08:48:47 +0200

    Update Yatzy.cs

--- csharp/Yatzy/Yatzy.cs
+++ csharp/Yatzy/Yatzy.cs
@@ -2,23 +2,23 @@ namespace Yatzy
 {
     public class Yatzy
     {
-        public static int Chance(int d1, int d2, int d3, int d4, int d5)
+        public static int Chance(int dice1, int dice2, int dice3, int dice4, int dice5)
         {
             var total = 0;
-            total += d1;
-            total += d2;
-            total += d3;
-            total += d4;
-            total += d5;
+            total += dice1;
+            total += dice2;
+            total += dice3;
+            total += dice4;
+            total += dice5;
             return total;
         }
 
-        public static int Ones(int d1, int d2, int d3, int d4, int d5)
+        public static int Ones(int dice1, int dice2, int dice3, int dice4, int dice5)
         {
             return 0;
         }
         
-        public static int Twos(int d1, int d2, int d3, int d4, int d5)
+        public static int Twos(int dice1, int dice2, int dice3, int dice4, int dice5)
         {
             return 0;
         }
